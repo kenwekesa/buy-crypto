aea744ead41132d58d96958f6147bf05b008b335
Kennedy <ken@localhost.localdomain>
2022-03-13 22:56:41 +0300

    centered content

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <!Doctype html>
 <html>
 <head>
+
+   <style>
+        .container {
+  margin: 0;
+  top: 40%;
+  left: 30%;
+  width: 35%;
+  border: 3px solid green;
+  padding: 10px;
+  position: absolute;
+}
+   </style>
 </head>
 <body>
-
-<h2> Hey, this is cripto-mate.</h2>
+<div class ="container">
+<h1> Hey, this is cripto-mate.</h1>
 <p> We are don't have much now, come next time, you will find much :):)</p>
+</div>
 </body>
 </html> 
