56490a74a4768abd2db81dfb72b966a5ad3a9173
Kennedy <ken@localhost.localdomain>
2022-03-16 00:09:00 +0300

    Navbar added

--- index.php
+++ index.php
@@ -2,22 +2,109 @@
 <html>
 <head>
 
-   <style>
-        .container {
-  margin: 0;
-  top: 40%;
-  left: 30%;
-  width: 35%;
-  border: 3px solid green;
-  padding: 10px;
-  position: absolute;
+  
+   
+   
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <style> 
+   body::before {
+  display: block;
+  content: '';
+  height: 60px;
 }
-   </style>
+
+.enrol
+{
+  border-radius:0;
+}
+
+</style>
 </head>
 <body>
-<div class ="container">
-<h1> Hey, this is Buy Pal.</h1>
+
+
+
+<!-- Navbar -->
+<!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 fixed-top shadow">
+      <div class="container">
+        <a href="#" class="navbar-brand">Buy-Pal</a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navmenu"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navmenu">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link">Newbie Investor</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Market</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">News</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Membership</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">About us</a>
+            </li>
+            
+            <li class="nav-item">
+              <a href="#" class="nav-link">Contact us</a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    <!-- Showcase -->
+    <section
+      class="bg-dark text-light p-5 p-lg-4 pt-lg-5 text-center text-sm-start "
+    >
+      <div class="container">
+        <div class="d-sm-flex align-items-center justify-content-between">
+          <div>
+            <h1>Take it  the <span class="text-warning"> Right way here </span></h1>
+            <p class="lead my-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iure autem recusandae, veniam, illo dolor soluta assumenda minima quia velit officia sit exercitationem nam ipsa, repellendus aut facilis quasi voluptas!
+            </p>
+            <button
+              class="btn btn-primary btn-lg enrol"
+              data-bs-toggle="modal"
+              data-bs-target="#enroll"
+            >
+              Become a member
+            </button>
+          </div>
+          <img
+            class="img-fluid w-50 d-none d-sm-block"
+            src="img/showcase.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+
+
+<!-- Navbar -->
+
+<br/><br/><br/><br/>
+<h1> Hey, this is cripto-mate.</h1>
 <p> We are don't have much now, come next time, you will find much :):)</p>
-</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html> 
